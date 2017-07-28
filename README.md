79f3c3b3d17a259050fbdf811e76174de3fdb4b2
Chandrasekaran Selvarasu <fosstamil@gmail.com>
2017-07-28 11:22:44 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+
+A boilerplate for a React - Express - Babel - Webpack2 project.
+
---
+++ webpack.config.babel.js
@@ -0,0 +1,80 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import {HotModuleReplacementPlugin} from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const defaultEnv = {
+    dev: true,
+    production: false,
+};
+
+export default (env = defaultEnv) => ({
+  entry: [
+    ...env.dev ? [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:8080',
+    ] : [],
+    path.join(__dirname, 'app/index.jsx'),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    ...env.dev ? [
+      new HotModuleReplacementPlugin(),
+    ] : [
+      new ExtractTextPlugin('[name].css'),
+    ],
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.join(__dirname, 'app/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'eslint-loader',
+          }
+        ]
+      },
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'app'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: [
+                ['es2015', { modules: false }],
+                'react',
+              ],
+              plugins: ['react-hot-loader/babel']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        loader: env.dev ? 'style-loader!css-loader!sass-loader' : ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          loader: 'css!sass'
+        })
+      },
+    ]
+  },
+  devServer: {
+    hot: env.dev
+  },
+  resolveLoader: {
+  	moduleExtensions: ["-loader"]
+  },
+  devtool: env.dev ? 'cheap-module-eval-source-map' : 'cheap-module-source-map',
+});
